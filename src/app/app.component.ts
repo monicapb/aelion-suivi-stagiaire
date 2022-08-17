580f9143d1f60186978c5ed98a0db93cadac3971
monicapb <monica.pina.baracaldo@gmail.com>
2022-08-17 14:45:07 +0200

    UPDATE move logic from appComponent to internTableComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Logger } from './core/helpers/logger';
-import { Intern } from './core/models/intern';
 
 @Component({
   selector: 'app-root',
@@ -10,71 +8,7 @@ import { Intern } from './core/models/intern';
 export class AppComponent {
   public title = 'Hello Angular 13';
 
-  private static sortOrder: number = 1;
-
-  public intern: Intern = {
-    name: 'Aubert',
-    firstname: 'Jean-Luc',
-    email: 'jla.webprojet@gmail.com',
-    phoneNumber: '+(33)6 45 78 96 32'
-  }
-
-  public interns: Intern[] = [
-    this.intern,
-    {
-      name: 'Pina',
-      firstname: 'Monica',
-      email: 'cbien@hotmail.fr',
-      phoneNumber: '+(33)6 45 78 96 32'
-    },
-    {
-      name: 'Castanie',
-      firstname: 'Piotr',
-      email: 'pedrito@caramba.com',
-      phoneNumber: '+(33)6 45 78 96 32'
-    }
-  ]
   public getTitle(): string {
     return this.title;
-  }
-
-  public onDelete(intern: Intern): void {
-    /**
-    const remainingInterns: Intern[] = [];
-
-    for(let inArrayIntern of this.interns) {
-      if (inArrayIntern !== intern) {
-        remainingInterns.push(inArrayIntern);
-      }
-    }
-    this.interns = remainingInterns;
-    */
-
-    this.interns.splice(
-      this.interns.indexOf(intern),
-      1
-    );
-
-  }
-
-  public sortByName(): void {
-    Logger.info(`Before sort, sortOrder is : ${AppComponent.sortOrder}`);
-    this.interns.sort(
-      AppComponent.sortName
-    );
-    AppComponent.sortOrder = AppComponent.sortOrder * -1;
-    console.log(`After sort, sortOrder is : ${AppComponent.sortOrder}`);
-  }
-
-  private static sortName(intern1: Intern, intern2: Intern): number {
-    if (intern1.name > intern2.name) {
-      return 1 * AppComponent.sortOrder;
-    } else if (intern1.name < intern2.name) {
-      return -1 * AppComponent.sortOrder;
-    } else {
-      return 0;
-    }
-
-
   }
 }
--- src/app/intern/components/intern-table/intern-table.component.ts
+++ src/app/intern/components/intern-table/intern-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Logger } from './../../../core/helpers/logger';
+import { Intern } from './../../../core/models/intern';
 
 @Component({
   selector: 'app-intern-table',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intern-table.component.scss']
 })
 export class InternTableComponent implements OnInit {
+  private static sortOrder: number = 1;
+
+  public interns: Intern[] = [
+    {
+      name: 'Pina',
+      firstname: 'Monica',
+      email: 'cbien@hotmail.fr',
+      phoneNumber: '+(33)6 45 78 96 32'
+    },
+    {
+      name: 'Castanie',
+      firstname: 'Piotr',
+      email: 'pedrito@caramba.com',
+      phoneNumber: '+(33)6 45 78 96 32'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onDelete(intern: Intern): void {
+    this.interns.splice(
+      this.interns.indexOf(intern),
+      1
+    );
+
+  }
+
+  public sortByName(): void {
+    Logger.info(`Before sort, sortOrder is : ${InternTableComponent.sortOrder}`);
+    this.interns.sort(
+      InternTableComponent.sortName
+    );
+    InternTableComponent.sortOrder = InternTableComponent.sortOrder * -1;
+    console.log(`After sort, sortOrder is : ${InternTableComponent.sortOrder}`);
+  }
+
+  private static sortName(intern1: Intern, intern2: Intern): number {
+    if (intern1.name > intern2.name) {
+      return 1 * InternTableComponent.sortOrder;
+    } else if (intern1.name < intern2.name) {
+      return -1 * InternTableComponent.sortOrder;
+    } else {
+      return 0;
+    }
+  }
 }
