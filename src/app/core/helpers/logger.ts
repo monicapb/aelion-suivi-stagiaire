ec589a8d4df8fe9e2730335c2a85b173bc051a69
monicapb <monica.pina.baracaldo@gmail.com>
2022-08-17 13:25:55 +0200

    Create custom logger

---
+++ src/app/core/helpers/logger.ts
@@ -0,0 +1,30 @@
+import * as moment from 'moment';
+
+export class Logger {
+  public static globalStyle: string [] = [
+    'color: white',
+    'font-size: 16px'
+  ]
+  public static infoStyle: string[] = [
+    'background: blue'
+  ];
+
+  public static info(message: string): void {
+    console.log(Logger.messageBuilder(message), Logger.styleBuilder(Logger.infoStyle));
+  }
+
+  public static error(message: string): void {}
+
+  public static warning(message: string): void {}
+
+  private static messageBuilder(message: string): string {
+    const currentMomentDate: moment.Moment = moment();
+    const currentStringTime: string = currentMomentDate.format('h:mm:ss');
+
+    return `${currentStringTime} : %c${message}`;
+  }
+
+  private static styleBuilder(customStyle: string[]): string {
+    return Logger.globalStyle.concat(customStyle).join(';');
+  }
+}
