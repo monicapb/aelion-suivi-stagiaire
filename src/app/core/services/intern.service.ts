36f9c835069eab146ccc9e0b430cd5a4a397ea62
monicapb <monica.pina.baracaldo@gmail.com>
2022-08-18 11:48:43 +0200

    UPDATE : Accord Service to Interface

--- src/app/core/services/intern.service.ts
+++ src/app/core/services/intern.service.ts
@@ -1,34 +1,76 @@
 import { Injectable } from '@angular/core';
 import { Logger } from '../helpers/logger';
+import { ICrud } from '../interfaces/i-crud';
 import { Intern } from '../models/intern';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InternService {
+export class InternService implements ICrud<Intern> {
   public interns: Intern[] = [
     {
+      id: 125,
       name: 'Aubert',
       firstname: 'Jean-Luc',
       email: 'jla.webprojet@gmail.com',
-      phoneNumber: '+(33)6 23 56 89 54'
+      phoneNumber: '+(33)6 23 56 89 54',
+      birthDate: new Date(1968, 3, 30),
+      address: '10 rue du Stade'
     },
     {
+      id: 2,
       name: 'Pina',
       firstname: 'Monica',
       email: 'cbien@hotmail.fr',
-      phoneNumber: '+(33)6 45 78 96 32'
+      phoneNumber: '+(33)6 45 78 96 32',
+      birthDate: new Date(1995, 10, 26),
+      address: '15 place de la République'
     },
     {
+      id: 3,
       name: 'Castanie',
       firstname: 'Piotr',
       email: 'pedrito@caramba.com',
-      phoneNumber: '+(33)6 45 78 96 32'
+      phoneNumber: '+(33)6 45 78 96 32',
+      birthDate: new Date(1998, 8, 23),
+      address: '1, place du Champ de Mars'
     }
   ];
 
   constructor() {
     Logger.info(`I'm a Singleton`);
+  }
+
+  findAll(): Intern[] {
+    // Your logic here !
+    return this.interns;
+  }
+
+  findOne(id: number): Intern | null {
+    const intern: Intern | undefined = this.interns.find(
+      (obj: Intern) => obj.id === id
+    );
+
+    /**
+     * let internFound: Intern | undefined = undefined;
+     *
+     * for (const intern of this.interns) {
+     *  if (intern.id === id) {
+     *    internFound = intern;
+     *    break;
+     *  }
+     * }
+     *
+     */
+    return (intern === undefined) ? null : intern;
+
+    /**
+    if (intern === undefined) {
+      return null;
+    }
+    return intern;
+    **/
+
   }
 
   public getItemNumber(): number {
@@ -42,11 +84,28 @@ export class InternService {
     );
   }
 
-  public add(intern: Intern): void {}
+  public add(intern: Intern): void {
+   if (this.findOne(intern.id!) === null) {
+    this.interns.push(intern);
+   }
+  }
 
-  public update(intern: Intern): void {}
+  public update(intern: Intern): void {
+    let oldIntern: Intern | null = this.findOne(intern.id!);
+    if (oldIntern !== null) {
+      oldIntern.name = intern.name;
+      oldIntern.firstname = intern.firstname;
+      oldIntern.address = intern.address;
+      oldIntern.birthDate = intern.birthDate;
+      oldIntern.email = intern.email;
+      oldIntern.phoneNumber = intern.phoneNumber;
 
-  public getAll(): Intern[] {
+      // oldIntern = {...intern};
+    }
+
+  }
+
+  public gotOne(): Intern[] {
     return this.interns;
   }
 
