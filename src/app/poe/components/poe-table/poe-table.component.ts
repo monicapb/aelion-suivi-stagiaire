ab6acdfd531c0404d9d92e7ae64830021926806c
monicapb <monica.pina.baracaldo@gmail.com>
2022-08-20 18:18:35 +0200

    implement poe table

--- src/app/poe/components/poe-table/poe-table.component.ts
+++ src/app/poe/components/poe-table/poe-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { POEService } from 'src/app/core/services/poe.service';
 
 @Component({
   selector: 'app-poe-table',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class POETableComponent implements OnInit {
 
-  constructor() { }
+  constructor(public poeService : POEService) { }
 
   ngOnInit(): void {
   }
