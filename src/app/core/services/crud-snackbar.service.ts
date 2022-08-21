ae706af7a199571b0d31b7b6430a699686c7ebee
DaCodeManiaK <jla.webprojet@gmail.com>
2022-08-21 09:04:33 +0200

    UPDATE : add config and open method

--- src/app/core/services/crud-snackbar.service.ts
+++ src/app/core/services/crud-snackbar.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudSnackbarService {
 
-  constructor() { }
+  private message: string;
+  private action?: string;
+
+  private static duration: number = 3;
+
+  constructor(private snackBar: MatSnackBar) {
+    this.message = 'message';
+    this.action = 'action';
+  }
+
+
+  config(message: string, action?: string) {
+    this.message = message;
+    this.action = action;
+  }
+
+  open(): void {
+    this.snackBar.open(
+      this.message,
+      this.action,
+      {
+        duration: CrudSnackbarService.duration * 1000
+      }
+    )
+  }
 }
