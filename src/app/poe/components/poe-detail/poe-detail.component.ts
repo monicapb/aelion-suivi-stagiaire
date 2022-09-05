450fec7f3130747d14ca70b16d69f88ceab36760
DaCodeManiaK <jla.webprojet@gmail.com>
2022-09-05 12:56:20 +0200

    FIX : Subscribe to service

--- src/app/poe/components/poe-detail/poe-detail.component.ts
+++ src/app/poe/components/poe-detail/poe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { POE } from 'src/app/core/models/poe';
 import { POEService } from 'src/app/core/services/poe.service';
-
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-poe-detail',
   templateUrl: './poe-detail.component.html',
@@ -19,7 +19,11 @@ export class POEDetailComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe(
       (paramMap: ParamMap) => {
         const id : number = +paramMap.get('id')!;
-        this.poe = this.poeService.findOne(id);
+        this.poeService.findOne(id).pipe(
+          take(1)
+        ).subscribe((poe: POE | null) => {
+          this.poe = poe;
+        });
       }
     )
   }
--- src/app/poe/components/poe-table/poe-table.component.ts
+++ src/app/poe/components/poe-table/poe-table.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { POE } from 'src/app/core/models/poe';
 import { POEService } from 'src/app/core/services/poe.service';
-
+import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-poe-table',
   templateUrl: './poe-table.component.html',
   styleUrls: ['./poe-table.component.scss']
 })
 export class POETableComponent implements OnInit {
-
+  public poes: POE[] = [];
   constructor(public poeService : POEService) { }
 
   ngOnInit(): void {
+    this.poeService.findAll().pipe(
+      take(1)
+    ).subscribe((poes: POE[]) => {
+      this.poes = poes;
+    })
   }
 
 }
