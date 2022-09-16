a9839e41bd1d2cf273db79107c665d84e1cbeba8
Jean-Luc <jean-luc.a@ideafactory.fr>
2022-09-16 09:14:44 +0200

    ADD guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { POEDetailComponent } from './poe/components/poe-detail/poe-detail.compo
 import { POETableComponent } from './poe/components/poe-table/poe-table.component';
 import { UserSigninComponent } from './user/components/user-signin/user-signin.component';
 import { HasUserGuard } from './user/guards/has-user.guard';
+import { NoUserGuard } from './user/guards/no-user.guard';
 
 @NgModule({
   imports: [RouterModule.forRoot(AppRoutingModule.routes)],
@@ -22,27 +23,45 @@ export class AppRoutingModule {
     },
     {
       path: 'interns',
-      component: InternTableComponent
+      component: InternTableComponent,
+      canActivate: [ // Pattern COF (Chain Of Responsabilities)
+        NoUserGuard
+      ]
     },
     {
       path: 'intern/:id',
-      component: InternDetailComponent
+      component: InternDetailComponent,
+      canActivate: [
+        NoUserGuard
+      ]
     },
     {
       path: 'intern/manage/add',
       component: InternAddComponent,
+      canActivate: [
+        NoUserGuard
+      ]
     },
     {
       path: 'poecollections',
-      component: POETableComponent
+      component: POETableComponent,
+      canActivate: [
+        NoUserGuard
+      ]
     },
     {
       path: 'poedetail/:id',
-      component: POEDetailComponent
+      component: POEDetailComponent,
+      canActivate: [
+        NoUserGuard
+      ]
     },
     {
       path: 'poe/manage/add',
       component: POEAddComponent,
+      canActivate: [
+        NoUserGuard
+      ]
     },
     {
       path: 'signin',
--- src/app/user/guards/no-user.guard.ts
+++ src/app/user/guards/no-user.guard.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HasUserGuard implements CanActivate {
+
+export class NoUserGuard implements CanActivate {
   public constructor(
-    private userService : UserService
+    private userService : UserService,
+    private router : Router
   ){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      console.log(this.userService.isAuthenticated());
-    return this.userService.isAuthenticated() ? false : true;
+      if(this.userService.isAuthenticated()) {
+        return true
+      }
+      this.router.navigateByUrl('/signin');
+      return false
+
   }
 
 }
